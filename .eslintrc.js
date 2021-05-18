164a793bfb243d196c6c01175b43ac0136eae9e0
parkjihwanjay <cc6656@maver.com>
2021-05-18 22:29:49 +0900

    [env][#3] eslint, prettier 설정
    - rules에 필요없는 rule들 off 설정
    - .eslintrc.js에 eslint 관련 설정들
    - .prettierrc에 prettier 관련 설정들

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['prettier', '@typescript-eslint', 'react-hooks'],
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended', // 설치 한경우
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-shadow': 'off',
+    'prettier/prettier': 'error',
+    'import/extensions': ['off'],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'no-use-before-define': 'off',
+    'react/prop-types': 'off',
+    'import/no-unresolved': 'off',
+    'react/jsx-pascal-case': 'off',
+    'no-alert': 'off',
+    'react/jsx-curly-newline': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
