07d608152487e68f7badfb19fd29ce7b4eb46bba
parkjihwanjay <cc6656@maver.com>
2021-05-18 22:33:25 +0900

    [env][#3] typescript 설정
    - tsconfig.json에 typescript 관련 설정
      - next.js에서 제공해주는 tsconfig 사용
    - js 파일들 -> tsx 파일로 변환
    - 매개변수, 반환값에 타입 추가
    - props에 타입추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import React from 'react';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -65,5 +66,7 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
