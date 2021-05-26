5b0a832ea9d3f4d87c0f45c59dbd2b699ed909a8
parkjihwanjay <cc6656@maver.com>
2021-05-27 00:28:22 +0900

    [feat][#20] _app.tsx에서 provider로 store 제공

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css';
 import '../styles/index.css';
+import { Provider } from 'react-redux';
+import store from '@/store';
 import type { AppProps } from 'next/app';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 };
 
 export default MyApp;
