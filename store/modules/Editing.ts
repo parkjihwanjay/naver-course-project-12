fddde284d161131de6bfb6848e275a8b40ee55fc
parkjihwanjay <cc6656@maver.com>
2021-06-03 17:48:39 +0900

    [feat][#47] editing state 생성
    - 일단은 editing하는 column id를 저장하도록
    - 이후 card 정보 또한 포함할 예정
    - 계속 추가되는 column, card에 isEditing을 저장하는 것보다는 현재 수정중인 column, card id를 저장하는게 더 효율적이라고 생각
    - Model과의 type 맞추기에도 용이
    - Component state로 해도 되나 이후 수정 중일때는 드래그가 안되는 식의 전개도 가능하기 때문에 전역 상태로 관리

---
+++ store/modules/Editing.ts
@@ -0,0 +1,23 @@
+import { createSlice, CreateSliceOptions, PayloadAction } from '@reduxjs/toolkit';
+import { Action } from 'typesafe-actions';
+
+type IEditingState = string | null;
+
+const reducers: CreateSliceOptions['reducers'] = {
+  setEditingState: (state: IEditingState, action: PayloadAction<string>) => {
+    console.log(action.payload);
+    return action.payload;
+  },
+};
+
+const editingSlice = createSlice({
+  name: 'editing',
+  initialState: null,
+  reducers,
+});
+
+const { setEditingState } = editingSlice.actions;
+
+export const setEditingStateAction = (payload: string): Action => setEditingState(payload);
+
+export default editingSlice.reducer;
