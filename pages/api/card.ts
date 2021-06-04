13965b39ac80d314c462905d95917eaa4eee7363
parkjihwanjay <cc6656@maver.com>
2021-06-04 22:27:53 +0900

    [feat][#55] addCard api 작성
    - columnId와 content를 인자로 받고 해당 column에 추가하는 api

--- pages/api/card.ts
+++ pages/api/card.ts
@@ -1,13 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import CardListModel from '@/data/model/CardListModel';
 import { ICardListModel } from '@/interfaces/api/card-list';
+import { findById } from '@/utils';
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
+  const cardList = CardListModel.readData<ICardListModel>();
+  const counter = CardListModel.readCounter();
   switch (req.method) {
     case 'PATCH': {
       const { list } = req.body;
       CardListModel.writeData<ICardListModel>(list);
       res.status(200).json(list);
+      break;
+    }
+    case 'POST': {
+      const { columnId, content } = req.body;
+      const { item } = findById(cardList, columnId);
+      const targetColumn = item;
+      const newCard = { content, id: `${counter + 1}` };
+      targetColumn.items.push({ content, id: `${counter + 1}` });
+      CardListModel.writeCounter(counter + 1);
+      CardListModel.writeData(cardList);
+      res.status(200).json(newCard);
       break;
     }
     default:
