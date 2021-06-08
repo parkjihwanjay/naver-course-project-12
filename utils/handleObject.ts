9a1ccfa5bce731d14336de95aa0e0a699bbe5945
parkjihwanjay <cc6656@maver.com>
2021-06-08 22:47:30 +0900

    [feat][#66] handlObject 함수 수정
    - generic으로 타입을 받도록

--- utils/handleObject.ts
+++ utils/handleObject.ts
@@ -1,4 +1,4 @@
-const updateValues = (original: Record<string, unknown>, toBeChanged: Record<string, unknown>) => {
+const updateValues = <T>(original: T, toBeChanged: Record<string, unknown>) => {
   const keys = Object.keys(toBeChanged);
   keys.forEach((key) => {
     original[key] = toBeChanged[key];
